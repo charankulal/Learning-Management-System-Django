4573a24eab02133e07662bee90c61b2fff55d202
Charan <charankulal0241@gmail.com>
2023-10-28 18:34:00 +0530

    Updated the project structure along with the urls and views and changed from rooms to courses.

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="Home"),
-    path('rooms/',views.rooms,name="Rooms")
+    path('courses/<str:pk>/',views.courses,name="Courses")
 ]
 
--- base/views.py
+++ base/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request,'home.html')
+course_dict=[
+    {
+        'id':1,
+        'name':'Lets learn python'
+        
+    },
+    {
+        'id':2,
+        'name':'Lets learn Java'
+        
+    },
+    {
+        'id':3,
+        'name':'Lets learn c'
+        
+    },
+    {
+        'id':4,
+        'name':'Lets learn c++'
+        
+    },
+    
+]
 
-def rooms(request):
-    return render(request,'rooms.html')
+def home(request):
+    context={'courses':course_dict}
+    return render(request,'base/home.html',context)
+
+def courses(request,pk):
+    courses=None
+    for course in course_dict:
+        if course['id']==int(pk):
+            courses=course
+            break
+    context={'courses':courses}
+    return render(request,'base/courses.html',context)
