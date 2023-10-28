0259c12be370de6e4eab8d87f40097c0babe074b
Charan <charankulal0241@gmail.com>
2023-10-28 21:39:39 +0530

    Updated

--- base/views.py
+++ base/views.py
@@ -10,5 +10,10 @@ def home(request):
 
 def courses(request,pk):
     course=Course.objects.get(id=pk)
+    teacher_name=course.teacher_id
+    teacher=Teacher.objects.get(name=teacher_name)
     context1={'course':course}
-    return render(request,'base/courses.html',context1)
+    
+    context2={'teacher':teacher}
+   
+    return render(request,'base/courses.html',context1,context2)
