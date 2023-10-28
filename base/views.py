998d58fe36699b3e1af4eefff54a6ec9db57ffcb
Charan <charankulal0241@gmail.com>
2023-10-28 20:50:16 +0530

    Created home page.

--- base/views.py
+++ base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from .models import Course,Teacher,Topic,Student,PurchaseAndEnrolment
 
 
 
 def home(request):
-    context={'courses':course_dict}
+    course=Course.objects.all()
+    context={'course':course}
     return render(request,'base/home.html',context)
 
 def courses(request,pk):
-    courses=
-    context1={'courses':courses}
+    course=Course.objects.get(id=pk)
+    context1={'course':course}
     return render(request,'base/courses.html',context1)
